2a612d63654047f348ec31ba77691092285a3b10
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2022-05-24 22:00:13 -0300

    Creacion entidad Experencia

--- src/main/java/com/prueba/SpringBoot/models/Experiencia.java
+++ src/main/java/com/prueba/SpringBoot/models/Experiencia.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.prueba.SpringBoot.models;
 
-/**
- *
- * @author varel
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
+@Entity
 public class Experiencia {
     
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long  id;
+    
+    
+    private String periodo;
+    private String nombre;
+    private String puesto;
+    private String tareas;
+    private String tipo;
+    private boolean actual;
+    
+    
+     public Experiencia() {
+    }
+
+    public Experiencia(Long id, String periodo, String nombre, String puesto, String tareas, String tipo, boolean actual) {
+        this.id = id;
+        this.periodo = periodo;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.tareas = tareas;
+        this.tipo = tipo;
+        this.actual = actual;
+    }
 }
