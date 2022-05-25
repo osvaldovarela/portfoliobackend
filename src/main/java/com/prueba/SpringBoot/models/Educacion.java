e56489b78e33961bf80a785db4ab37e11d4b4d0d
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2022-05-24 22:06:54 -0300

    Creacion entidad Educacion

--- src/main/java/com/prueba/SpringBoot/models/Educacion.java
+++ src/main/java/com/prueba/SpringBoot/models/Educacion.java
@@ -1,7 +1,38 @@
 
 package com.prueba.SpringBoot.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
+@Entity
 public class Educacion {
+    
+    @Id 
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    
+    //private long idIdioma;
+    private String institucion;
+    private String periodo;
+    private String  titulo;
+    private boolean completo;
+    private String descripcion;
+
+    public Educacion() {
+    }
+
+    public Educacion(Long id, String institucion, String periodo, String titulo, boolean completo, String descripcion) {
+        this.id = id;
+        this.institucion = institucion;
+        this.periodo = periodo;
+        this.titulo = titulo;
+        this.completo = completo;
+        this.descripcion = descripcion;
+    }
     
 }
